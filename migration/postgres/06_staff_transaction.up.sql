a82a69f5563277a9fac33d9cdf7d01927374717f
Otabek0811 <karimjonov6006@gmail.com>
2023-07-27 17:00:58 +0500

    Mini Project with GIN FrameWork, pgxpool and swagger--> Sales_Billing optimised

--- migration/postgres/06_staff_transaction.up.sql
+++ migration/postgres/06_staff_transaction.up.sql
@@ -4,6 +4,7 @@ CREATE TABLE staff_transaction(
     description VARCHAR ,
     price NUMERIC NOT NULL,
     sale_id UUID REFERENCES sale(id),
+    staff_id  UUID REFERENCES staff(id),
     source_type VARCHAR NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
